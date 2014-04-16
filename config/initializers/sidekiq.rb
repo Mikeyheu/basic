fdd9f585c8d8bcbc97a9628418135321eff1a311
Michael Heu <mheu@clickbooq.com>
2014-04-16 16:57:58 -0700

    latest

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://ec2-54-187-54-142.us-west-2.compute.amazonaws.com:6379/', :namespace => 'namespace' }
+  config.redis = { :url => 'redis://ec2-54-187-54-142.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://ec2-54-187-54-142.us-west-2.compute.amazonaws.com:6379/', :namespace => 'namespace' }
+  config.redis = { :url => 'redis://ec2-54-187-54-142.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
 end
