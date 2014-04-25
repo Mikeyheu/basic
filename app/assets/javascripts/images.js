c7ee47a489d652772c1d6e7bc31456b4a7f1599d
Michael Heu <mheu@clickbooq.com>
2014-04-25 16:58:16 -0700

    added progress bar uploader test

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-  $('#new_image').fileupload();
+  $('#new_image').fileupload({
+    dataType: 'json',
+    add: function(e, data){
+      // $.each(data.files, function (index, file) {
+      //   console.log('Added file: ' + file.name);
+      //   data.context = $(".progress-bar div");
+      //   data.submit();
+      // });
+      $('.progress_bar_wrapper').append($('.progress_context:first').clone());
+      data.context = $('.progress_context:last');
+      data.context.find($('.upload_file_name')).html(data.files[0].name);
+      data.context.show('fade');
+      data.submit();
+    },
+    progress: function (e, data) {
+      if (data.context) {
+        progress = parseInt(data.loaded / data.total * 100, 10);
+        data.context.find($('.progress-bar')).find('div').css('width',  progress + '%').find('span').html(progress + '%');
+      }
+    }
+  });
 });
