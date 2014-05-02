062079aca6e54582eb07f95d3f7ad9643f668f27
Michael Heu <mheu@clickbooq.com>
2014-05-02 12:30:17 -0700

    added local_env.yml file edits

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,11 @@ module Basic
     # config.i18n.default_locale = :de
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      if File.exists?(env_file)
+        YAML.load_file(env_file)[Rails.env].each do |key, value|
+          ENV[key.to_s] = value
+        end 
+      end
     end
   end
 end
