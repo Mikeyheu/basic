72a7c15e26eb9705436be96388ca2a13866d81d2
Michael Heu <mheu@clickbooq.com>
2014-05-02 12:32:15 -0700

    added local_env.yml file edits- test env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS', # required
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :aws_access_key_id      => 'AKIAIC64N6SHUP3XIEBA', # required
+      :aws_secret_access_key  => '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',  # required
     }
-    config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
+    config.fog_directory  = 'mheutest'                     # required
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
