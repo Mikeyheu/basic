f797b2587a24f0d54fc2cd28104b749cee514c13
Michael Heu <mheu@clickbooq.com>
2014-05-02 09:53:55 -0700

    using sidekiq 3.0.0 and removing sidekiq_status

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,28 +1,12 @@
 require 'sidekiq'
-require 'sidekiq-status'
-
-Sidekiq.configure_client do |config|
-  config.client_middleware do |chain|
-    chain.add Sidekiq::Status::ClientMiddleware
-  end
-end
-
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
-  end
-  config.client_middleware do |chain|
-    chain.add Sidekiq::Status::ClientMiddleware
-  end
-end
 
 if Rails.env =="production"
   Sidekiq.configure_server do |config|
-    config.redis = { :url => 'redis://ec2-54-187-54-142.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
+    config.redis = { :url => 'redis://ec2-54-187-158-134.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { :url => 'redis://ec2-54-187-54-142.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
+    config.redis = { :url => 'redis://ec2-54-187-158-134.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
   end
 end
 
