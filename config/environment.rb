579fc3689d8d2096ee1999a135e58943a31511e8
Michael Heu <mheu@clickbooq.com>
2014-05-04 12:51:17 -0700

    changes to code

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  if File.exists?(env_file)
-    puts "IT EXISTS!!!"
-    YAML.load_file(env_file)[Rails.env].to_hash.each do |key, value|
-      ENV[key.to_s] = value
-    end 
-  end
-
 # Initialize the Rails application.
 Basic::Application.initialize!
