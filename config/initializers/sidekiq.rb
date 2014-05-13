b10a246bae20d56c0d53ab9fe0a1df51cc450965
Michael Heu <mheu@clickbooq.com>
2014-05-13 12:52:26 -0700

    added rediscloud tweaks to env and url structure

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,15 +3,14 @@ require 'sidekiq'
 if Rails.env =="production"
   Sidekiq.configure_server do |config|
     # config.redis = { :url => 'redis://ec2-54-187-158-134.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
-    config.redis = { :url => "redis://#{ENV['REDISCLOUD_PASSWORD']}@#{ENV['REDISCLOUD_URL']}", :namespace => 'namespace' }
+    config.redis = { :url => "redis://:#{ENV['REDISCLOUD_PASSWORD']}@#{ENV['REDISCLOUD_URL']}", :namespace => 'namespace' }
   end
 
   Sidekiq.configure_client do |config|
     # config.redis = { :url => 'redis://ec2-54-187-158-134.us-west-2.compute.amazonaws.com:6379', :namespace => 'namespace' }
     # config.redis = { :url => ENV['REDISCLOUD_URL'], :namespace => 'namespace', :password => ENV['REDISCLOUD_PASSWORD'] }
-    puts "URL:"
-    puts "redis://#{ENV['REDISCLOUD_PASSWORD']}@#{ENV['REDISCLOUD_URL']}"
-    config.redis = { :url => "redis://#{ENV['REDISCLOUD_PASSWORD']}@#{ENV['REDISCLOUD_URL']}", :namespace => 'namespace' }
+
+    config.redis = { :url => "redis://:#{ENV['REDISCLOUD_PASSWORD']}@#{ENV['REDISCLOUD_URL']}", :namespace => 'namespace' }
   end
 end
 
