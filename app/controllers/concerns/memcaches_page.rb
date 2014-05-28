09b99f3b3bd7c320d7b40b011f9b0cf6a121d4dd
Michael Heu <mheu@clickbooq.com>
2014-05-28 11:21:25 -0700

    changed memcaches_page on sitescontroller

--- app/controllers/concerns/memcaches_page.rb
+++ app/controllers/concerns/memcaches_page.rb
@@ -11,16 +11,21 @@ module MemcachesPage
     end
 
     def memcache_page(content, host, path, options={})
+      puts "hit class method memcache_page"
       return unless perform_caching
 
       cache_path = host << path
-
+      puts "attempting to write to cache"
       Rails.cache.write cache_path.gsub('%', '%25'), content, nil # options.merge(raw: true)
+      puts "Dump of the cache"
+      puts Rails.cache
     end
   end
 
   def memcache_page(options = {})
+    puts "hit memcache_page instance method"
     return unless self.class.perform_caching && caching_allowed? && !request.params.key?('no-cache')
+    puts "passed return on memcache_page instance method"
     self.class.memcache_page(response.body, request.host, request.fullpath, options)
   end
 end
