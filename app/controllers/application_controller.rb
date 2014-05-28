b50a1289599cb86cceb94f50f7cc978da8ca14fe
Michael Heu <mheu@clickbooq.com>
2014-05-28 11:07:48 -0700

    trying memcached page code again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # include MemcachesPage
+  include MemcachesPage
 
-  def cache_page(content = nil, options = nil, gzip = Zlib::BEST_COMPRESSION)
-  	puts "YOU ARE GETTING HIT!!!!"
-    path = "/#{request.host}"
-    path << case options
-    when Hash
-      url_for(options.merge(:only_path => true, :skip_relative_url_root => true, :format => params[:format]))
-    when String
-      options
-    else
-      if request.path.empty? || request.path == '/'
-        '/index'
-      else
-        request.path
-      end
-    end
-    # super(content, path, gzip)
-    Rails.cache.write path.gsub('%', '%25'), content, nil
-  end
+  # def cache_page(content = nil, options = nil, gzip = Zlib::BEST_COMPRESSION)
+  # 	puts "YOU ARE GETTING HIT!!!!"
+  #   path = "/#{request.host}"
+  #   path << case options
+  #   when Hash
+  #     url_for(options.merge(:only_path => true, :skip_relative_url_root => true, :format => params[:format]))
+  #   when String
+  #     options
+  #   else
+  #     if request.path.empty? || request.path == '/'
+  #       '/index'
+  #     else
+  #       request.path
+  #     end
+  #   end
+  #   # super(content, path, gzip)
+  #   Rails.cache.write path.gsub('%', '%25'), content, nil
+  # end
 
 end
