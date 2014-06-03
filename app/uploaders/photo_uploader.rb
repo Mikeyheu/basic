dae38251e3b415f183290ac1ac65f7478d9b242d
Michael Heu <mheu@clickbooq.com>
2014-06-03 16:25:45 -0700

    first commit

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,7 +2,8 @@
 
 class PhotoUploader < CarrierWave::Uploader::Base
 
-  include ::CarrierWave::Backgrounder::Delay
+  # include ::CarrierWave::Backgrounder::Delay
+  include CarrierWaveDirect::Uploader
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -14,27 +15,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
 
   # Create different versions of your uploaded files:
   version :thumb do
